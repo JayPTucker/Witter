e49a548b94f8ab49cb50e87731d5293e04172a51
Jay Paul Tucker <jaypaultucker@gmail.com>
2023-12-06 14:21:39 -0600

    Delete index.js

--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function() {
-//     console.log("index.js works")
-
-//     $.get("/api/user_data").then(function(data) {
-//         $(".user-name").text(data.username);
-//     })
-// });
