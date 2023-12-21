fffe8adccc08afa438d2d4814e77ce194007e7a0
Jay Paul Tucker <jaypaultucker@gmail.com>
2023-12-20 21:05:17 -0600

    More CSS Changes

--- public/js/witter.js
+++ public/js/witter.js
@@ -90,17 +90,18 @@ function displayWit(wit) {
                     <img class="Wit-profilePic" src="/uploads/${profilePicture}">
                 </div>
 
-                <div class="col-md-9">
-                    <h4 class="wit-author">@${wit.author}</h4>
+                <div class="col-md-10">
+                    <h4 class="wit-author">@${wit.author}</h4><p class="wit-date">${moment(wit.createdAt).format("h:mma on dddd")} </p>
                     <p class="wit-body">${wit.body}</p>
                     <button type="button" data-wit-id="${wit.id}" class="like-button wit-like-btn btn btn-default btn-sm">
-                        <span class="glyphicon glyphicon-thumbs-up"></span> 
-                        ${getLikeCount(wit.likes)} Likes
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
+                        <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
+                    </svg>
+                        ${getLikeCount(wit.likes)}
                     </button>
                 </div>
 
-                <div class="col-md-2">
-                    <p class="wit-date">${moment(wit.createdAt).format("h:mma on dddd")} </p>
+                <div class="col-md-1">
                     <div class="dropdown-container"></div> <!-- Container for dropdown -->
                 </div>
             </div>
@@ -178,7 +179,11 @@ function renderDropDown(wit, row) {
         } else if (authorInput === wit.author) {
             return `
                 <div class="dropdown">
-                    <button class="dropbtn">Options</button>
+                    <button class="dropbtn">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                            <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
+                        </svg>
+                    </button>
                     <div class="dropdown-content">
                         <a class="edit-button" href="#">Edit</a>
                         <a class="delete-button" href="#">Delete</a>
