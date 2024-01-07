19d54447b998387a6b01859c6387c96139806196
Jay Paul Tucker <jaypaultucker@gmail.com>
2024-01-07 17:13:46 -0600

    Fixed Trending Section with new API-Routes
    - Adjusted Trending Section with new organization metrics

--- public/js/witter.js
+++ public/js/witter.js
@@ -7,7 +7,6 @@ $.get("/api/user_data").then(function(user) {
     var authorInput = user.username;
     var bodyInput = $("#wit-input");
     var imageInput = $("#image-input");
-
     var newWitProfilePic = user.profilePicture;
 
     function loadCurrentProfilePic() {
@@ -60,6 +59,10 @@ $.get("/api/user_data").then(function(user) {
                                 ${likesCount}
                                 </button>
                             </div>
+
+                            <div class="col-md-1">
+                                <div class="dropdown-container"></div> <!-- Container for dropdown -->
+                            </div>
                         </div>
                         `);
 
@@ -95,6 +98,16 @@ $.get("/api/user_data").then(function(user) {
                             row.find('.wit-like-btn').css('color', 'white');
                         }
 
+                        renderDropDown(data, row).then((dropdownHtml) => {
+                            row.find('.dropdown-container').html(dropdownHtml);
+            
+                            row.find('.edit-button').on('click', function () {
+                                handleEditButtonClick(wit.id, data.username, row);
+                            });
+                            row.find('.delete-button').on('click', function () {
+                                handleDeleteButtonClick(wit.id, data.username, row);
+                            });
+                        });
                     }
                 })
             .catch(error => {
@@ -111,9 +124,13 @@ $.get("/api/user_data").then(function(user) {
     function loadTrendingWits() {
         $.get("/api/top_wits").then(function(data) {
             if (data.length !== 0) {
-                
-                data.reverse(); 
-                for (var i = 0; i < data.length; i++) {
+                // Create an array of promises for fetching profile pictures
+                const profilePicturePromises = data.map(wit => findProfilePicture(wit.author));
+
+                // Wait for all promises to resolve
+                Promise.all(profilePicturePromises)
+                .then(profilePictures => {
+                    for (var i = 0; i < data.length; i++) {
                     // Parse the likes string into a JSON array if it's not empty
                     var likesArray;
                     try {
@@ -125,47 +142,60 @@ $.get("/api/user_data").then(function(user) {
 
                     // Get the length of the array
                     var likesCount = likesArray.length;
-                    
-                    var row = $(`<div class="T-wit-row col-md-12" id="wit-${data[i].id}"></div>`);
-                    row.append(`
-                    <div class="row">
-                        <div class="col-md-3">
+
+                        var mainData = data[i];
+                        var row = $(`<div class="T-wit-row col-md-12" id="wit-${data[i].id}"></div>`);
+                        row.append(`
+                        <div class="row">
+
+                            <div class="col-md-3" id="T-witProfilePic"></div>
+            
+                            <div class="col-md-9">
+                                <h4 class="T-wit-author">@${data[i].author}</h4><p class="T-wit-date">${moment(data[i].createdAt).format("h:mma on dddd")} </p>
+                                <p class="T-wit-body">${data[i].body}</p>
+                                <button type="button" data-wit-id="${data[i].id}" class="T-wit-like-btn btn btn-default btn-sm">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
+                                        <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
+                                    </svg>
+
+                                    ${likesCount}
+
+                                </button>
+                            </div>
+
                         </div>
-        
-                        <div class="col-md-9">
-                            <h4 class="T-wit-author">@${data[i].author}</h4><p class="T-wit-date">${moment(data[i].createdAt).format("h:mma on dddd")} </p>
-                            <p class="T-wit-body">${data[i].body}</p>
-                            <button type="button" data-wit-id="${data[i].id}" class="T-wit-like-btn btn btn-default btn-sm">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
-                                <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
-                            </svg>
-                            ${likesCount}
-                            </button>
-                        </div>
-                    </div>
-                    `);
-        
-                    $("#trendingWits").prepend(row);
-        
-                    // Check if there is an image
-                    if (data[i].image) {
-                        // Display the image in the new row
-                        displayImage(data[i].image, row);
-                    }
-        
-                    // && Checks to see if the array is empty or not or if it includes the username
-                    // if (data[i].likes && data[i].likes.includes(data[i].username)) {
-                    //     row.find('.T-wit-like-btn').css('background-color', 'red');
-                    //     row.find('.T-wit-like-btn').css('color', 'white');
-                    // } 
-        
-                    // Attach click event handler to the like button
-                    row.find('.T-wit-like-btn').on('click', function () {
+                        `);
+
+                        $("#trendingWits").prepend(row);
+
+                        // Append profile pic to the page
+                        row.find('#T-witProfilePic').html(`<img class="T-Wit-profilePic" src="/uploads/${profilePictures[i]}"></img>`);
+
+                        // Attach click event handler to the like button
+                        row.find('.T-wit-like-btn').on('click', function () {
                         // Call the function to handle the like button click
-                        console.log(this.dataset.witId);
-                        likePost(this.dataset.witId, user.username, row);
-                });
-                }
+                            console.log(user);
+                            // THIS BELOW WILL LOG THE ID OF THE WIT YOU ARE CLICKING ON
+                            console.log(this.dataset.witId);
+                            likePost(this.dataset.witId, user.username, row);
+                        });
+
+                        // Check if there is an image
+                        if (data[i].image) {
+                            // Display the image in the new row
+                            displayImage(data[i].image, row);
+                        }
+
+                        const lowercaseUsername = user.username.toLowerCase();
+
+                        // && Checks to see if the array includes the username
+                        if (data[i].likes && data[i].likes.includes(lowercaseUsername)) {
+                            console.log('User liked the wit!');
+                            row.find('.T-wit-like-btn').css('background-color', 'red');
+                            row.find('.T-wit-like-btn').css('color', 'white');
+                        }
+                    }
+                })
             } else {
                 return;
             }
