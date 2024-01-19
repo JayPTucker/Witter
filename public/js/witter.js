cb72fb1dc982f179f0c13bdb92022522837575dc
Jay Paul Tucker <jaypaultucker@gmail.com>
2024-01-19 14:29:39 -0600

    Fixed Like Button
    - Like button fixed for old Wits
        - Now when you like or unlike a post it changes instantly on the page to reflect your like status rather than you having to refresh the page in the past.
    - New Wits like button needs to be fixed.

--- public/js/witter.js
+++ public/js/witter.js
@@ -29,85 +29,89 @@ $.get("/api/user_data").then(function(user) {
                 Promise.all(profilePicturePromises)
                 .then(profilePictures => {
                     for (var i = 0; i < data.length; i++) {
-                    // Parse the likes string into a JSON array if it's not empty
-                    var likesArray;
-                    try {
-                        likesArray = JSON.parse(data[i].likes || '[]');
-                    } catch (error) {
-                        console.error("Error parsing likes:", error);
-                        likesArray = [];
-                    }
-
-                    // Get the length of the array
-                    var likesCount = likesArray.length;
-
-                        var mainData = data[i];
-                        var row = $(`<div class="wit-row col-md-12" id="wit-${data[i].id}"></div>`);
-                        row.append(`
-                        <div class="row">
-                            <div class="col-md-3" id="witProfilePic">
-
-                            </div>
-            
-                            <div class="col-md-9">
-                                <h4 class="wit-author">@${data[i].author}</h4><p class="wit-date">${moment(data[i].createdAt).format("h:mma on dddd")} </p>
-                                <p class="wit-body">${data[i].body}</p>
-                                <button type="button" data-wit-id="${data[i].id}" class="wit-like-btn btn btn-default btn-sm">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
-                                    <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
-                                </svg>
-                                ${likesCount}
-                                </button>
-                            </div>
-
-                            <div class="col-md-1">
-                                <div class="dropdown-container"></div> <!-- Container for dropdown -->
-                            </div>
-                        </div>
-                        `);
-
-                        $("#wits-area").prepend(row);
-
-                        // Append profile pic to the page
-                        row.find('#witProfilePic').html(`<img class="Wit-profilePic" src="/uploads/${profilePictures[i]}"></img>`);
-
-                        // Attach click event handler to the like button
-                        row.find('.wit-like-btn').on('click', function () {
-                        // Call the function to handle the like button click
-                            console.log(user);
-                            // THIS BELOW WILL LOG THE ID OF THE WIT YOU ARE CLICKING ON
-                            console.log(this.dataset.witId);
-                            likePost(this.dataset.witId, user.username, row);
-                        });
-
-                        // Check if there is an image
-                        if (data[i].image) {
-                            // Display the image in the new row
-                            displayImage(data[i].image, row);
+                        // Parse the likes string into a JSON array if it's not empty
+                        var likesArray;
+                        try {
+                            likesArray = JSON.parse(data[i].likes || '[]');
+                        } catch (error) {
+                            console.error("Error parsing likes:", error);
+                            likesArray = [];
                         }
 
-                        console.log('user.username:', user.username);
-                        console.log('data[i].likes:', data[i].likes);
+                        // Get the length of the array
+                        var likesCount = likesArray.length;
 
-                        const lowercaseUsername = user.username.toLowerCase();
+                            var mainData = data[i];
+                            var row = $(`<div class="wit-row col-md-12" id="wit-${data[i].id}"></div>`);
+                            row.append(`
+                            <div class="row">
+                                <div class="col-md-3" id="witProfilePic">
 
-                        // && Checks to see if the array includes the username
-                        if (data[i].likes && data[i].likes.includes(lowercaseUsername)) {
-                            console.log('User liked the wit!');
-                            row.find('.wit-like-btn').css('background-color', 'red');
-                            row.find('.wit-like-btn').css('color', 'white');
-                        }
+                                </div>
+                
+                                <div class="col-md-9">
+                                    <h4 class="wit-author">@${data[i].author}</h4><p class="wit-date">${moment(data[i].createdAt).format("h:mma on dddd")} </p>
+                                    <p class="wit-body">${data[i].body}</p>
 
-                        renderDropDown(data, row).then((dropdownHtml) => {
-                            row.find('.dropdown-container').html(dropdownHtml);
-            
-                            row.find('.edit-button').on('click', function () {
-                                handleEditButtonClick(wit.id, data.username, row);
+                                    <button type="button" data-wit-id="${data[i].id}" class="wit-like-btn wit-like-btn-${data[i].id} btn btn-default btn-sm">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
+                                        <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
+                                    </svg>
+                                    ${likesCount}
+                                    </button>
+                                </div>
+
+                                <div class="col-md-1">
+                                    <div class="dropdown-container"></div> <!-- Container for dropdown -->
+                                </div>
+                            </div>
+                            `);
+
+                            $("#wits-area").prepend(row);
+
+                            // Append profile pic to the page
+                            row.find('#witProfilePic').html(`<img class="Wit-profilePic" src="/uploads/${profilePictures[i]}"></img>`);
+
+                            // Attach click event handler to the like button
+                            row.find('.wit-like-btn').on('click', function () {
+                            // Call the function to handle the like button click
+                                console.log(user);
+                                // THIS BELOW WILL LOG THE ID OF THE WIT YOU ARE CLICKING ON
+                                console.log(this.dataset.witId);
+                                likePost(this.dataset.witId, user.username, row);
                             });
-                            row.find('.delete-button').on('click', function () {
-                                handleDeleteButtonClick(wit.id, data.username, row);
+
+                            // Check if there is an image
+                            if (data[i].image) {
+                                // Display the image in the new row
+                                displayImage(data[i].image, row);
+                            }
+
+                            console.log('user.username:', user.username);
+                            console.log('data[i].likes:', data[i].likes);
+
+                            const lowercaseUsername = user.username.toLowerCase();
+
+                            // && Checks to see if the array includes the username
+                            if (data[i].likes && data[i].likes.includes(lowercaseUsername)) {
+                                console.log('User liked the wit!');
+                                row.find('.wit-like-btn').css('background-color', 'red');
+                                row.find('.wit-like-btn').css('color', 'white');
+                            }
+
+                            renderDropDown(data[i], row).then((dropdownHtml) => {
+                                row.find('.dropdown-container').html(dropdownHtml);
+                            
+                                row.find('.edit-button').on('click', function () {
+                                    handleEditButtonClick(data[i], user.username, row);
+                                });
+                            
+                                row.find('.delete-button').on('click', function () {
+                                    handleDeleteButtonClick(this.dataset.witId, user.username, row);
+                                    console.log(this)
+                                });
                             });
-                        });
+                        
                     }
                 })
             .catch(error => {
@@ -264,17 +268,21 @@ function findProfilePicture(author) {
     });
 }
 
-function likePost(witId, username, row) {
+function likePost(witId, username) {
+    console.log("Like button has been pressed");
+    console.log(witId)
+    console.log(username)
+    var row = $(`#wit-${witId}`);
+
     $.ajax({
         method: 'POST',
         url: `/api/wits/${witId}/like`,
         data: {username: username},
         success: function (response) {
-            console.log("Wit has been liked successfully");
-            alert("Wit has been liked successfully");
-        
+            var witBtn = (".wit-like-btn-" + witId)
+
             // Update the like button with the new like count
-            row.find('.wit-like-btn').html(`
+            row.find(witBtn).html(`
                 <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
                     <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
                 </svg>
@@ -283,8 +291,11 @@ function likePost(witId, username, row) {
 
             // Update the like button style if needed
             if (response.userAlreadyLiked) {
-                row.find('.wit-like-btn').css('background-color', 'red');
-                row.find('.wit-like-btn').css('color', 'white');
+                row.find(witBtn).css('background-color', 'red');
+                row.find(witBtn).css('color', 'white');
+            } else {
+                row.find(witBtn).css('background-color', 'white');
+                row.find(witBtn).css('color', 'black');
             }
 
         },        
@@ -294,19 +305,22 @@ function likePost(witId, username, row) {
     })
 }
 
-function renderDropDown(data, row) {
-    return $.get("/api/user_data").then(function(data) {
-        var authorInput = data.username;
+function renderDropDown(witData, row) {
+    console.log("Rendering dropdown for wit:", witData);
 
+    return $.get("/api/user_data").then(function(userData) {
+        console.log("User data:", userData);
+
+        var authorInput = userData.username;
         var currentDate = moment();
-        var witCreationTime = moment(data.createdAt);
+        var witCreationTime = moment(witData.createdAt);
         var timeDifference = currentDate.diff(witCreationTime, 'minutes'); // Difference in minutes
 
-        if (authorInput !== data.author) {
-            return ""; // Return an empty string if not matching user
-
-        // Edit button disappears if time is less than 2 minutes.
+        if (authorInput !== witData.author) {
+            console.log("Author mismatch, returning empty string");
+            return "";
         } else if (timeDifference > 2) {
+            console.log("Rendering dropdown for wit created more than 2 minutes ago");
             return `
                 <div class="dropdown">
                     <button class="dropbtn">
@@ -318,7 +332,8 @@ function renderDropDown(data, row) {
                         <a class="delete-button" href="#">Delete</a>
                     </div>
                 </div>`;
-        } else if (authorInput === data.author) {
+        } else if (authorInput === witData.author) {
+            console.log("Rendering dropdown for wit created by the current user");
             return `
             <div class="dropdown">
                 <button class="dropbtn">
@@ -331,12 +346,14 @@ function renderDropDown(data, row) {
                     <a class="delete-button" href="#">Delete</a>
                 </div>
             </div>`;
-        }
+       }
     });
 }
 
 
-function handleDeleteButtonClick(witId, username) {
+
+
+function handleDeleteButtonClick(witData, username) {
     // Use a confirm dialog for user confirmation
     var userConfirmation = confirm("Are you sure you want to delete this Wit?");
 
@@ -354,7 +371,7 @@ function handleDeleteButtonClick(witId, username) {
 
     $.ajax({
         method: 'POST',
-        url: `/api/wits/${witId}/delete`,
+        url: `/api/wits/${witData}/delete`,
         data: {username: username},
         success: function (response) {
             console.log("Wit has been deleted successfully")
@@ -367,7 +384,7 @@ function handleDeleteButtonClick(witId, username) {
     }
 })
 
-function handleEditButtonClick(witId, username) {
+function handleEditButtonClick(witData, username) {
     console.log("edit button");
 
     // Use a confirm dialog for user confirmation
@@ -387,7 +404,7 @@ function handleEditButtonClick(witId, username) {
 
     $.ajax({
         method: 'POST',
-        url: `/api/wits/${witId}/edit`,
+        url: `/api/wits/${witData}/edit`,
         data: {editPrompt},
         success: function (response) {
             console.log("Wit has been edited successfully")
