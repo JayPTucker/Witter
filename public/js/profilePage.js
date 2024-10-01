cae0f3d56e421953780aef2f9e3dc1273c6c4248
Jay Paul Tucker <jaypaultucker@gmail.com>
2024-09-30 22:03:06 -0500

    Profile Page Changes
    - Made it look easier to read
    - Fixed Profile Pics not updating after change

--- public/js/profilePage.js
+++ public/js/profilePage.js
@@ -8,20 +8,28 @@ jQuery(function() {
         // Display current profile picture and username
         var row = $(`<div id="profilePicRow" class="profilePicRow"></div>`);
         row.append(`<p>Username: @${username}</p>`);
-        var profilePicElement = $(`<p>Profile Picture:</p><img class="profilePic" src="/uploads/${data.profilePicture}">`);
-        row.append(profilePicElement);
+
+        if (!data.profilePicture) {
+            console.log("No Profile Pic Set in the DB, using Default");
+            row.append(`<img class="currentProfilePic" src="/img/defaultProfilePic.png"></img>`);
+        } else {
+            console.log("Profile Pic is Set in the DB");
+            var profilePicElement = $(`<p>Profile Picture:</p><img class="currentProfilePic" src="/uploads/${data.profilePicture}">`);
+            row.append(profilePicElement);
+        }
+
         $(".user-info").prepend(row);
 
         newProfilePicForm.on("submit", function(event) {
             event.preventDefault();
-
+        
             if (imageInput[0].files[0] == null) {
                 alert("Please choose an image.");
             } else {
                 var formData = new FormData();
                 formData.append("username", username);
                 formData.append("profilePicture", imageInput[0].files[0]);
-
+        
                 $.ajax({
                     url: "/api/changeProfilePic",
                     type: "POST",
@@ -30,13 +38,15 @@ jQuery(function() {
                     contentType: false,
                     success: function (response) {
                         alert("Profile Picture changed successfully");
-
+        
                         // Update the profile picture on the page without refreshing
-                        // New profile picture URL from the response or update directly from the input
                         var newProfilePicUrl = `/uploads/${imageInput[0].files[0].name}`;
-
+        
                         // Update the profile picture on the page
                         profilePicElement.find("img").attr("src", newProfilePicUrl + `?t=${new Date().getTime()}`);
+                        
+                        // Optionally reload the page if you want to ensure the backend data is also updated
+                        window.location.reload(); // Uncomment this line if needed
                     },
                     error: function (err) {
                         alert("Profile Picture not properly changed.");
@@ -45,8 +55,7 @@ jQuery(function() {
                     }
                 });
             }
-
-            window.location.reload();
         });
+        
     });
 });
