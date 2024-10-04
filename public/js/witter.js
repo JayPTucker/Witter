fc53b34eee15565eb28d8d4cf761b9653646d6c4
Jay Paul Tucker <jaypaultucker@gmail.com>
2024-10-03 20:50:38 -0500

    CSS/HTML Format Changes for Images
    - Images are now in the right spot
    - Also has cool borders for a cleaner look.

--- public/js/witter.js
+++ public/js/witter.js
@@ -87,6 +87,7 @@ jQuery(function() {
                                     <h4 class="wit-author">@${data[i].author}</h4>
                                     <p class="wit-date">${moment(data[i].createdAt).format("h:mma on dddd")} </p>
                                     <p class="wit-body">${data[i].body}</p>
+                                    <p class="imgAttachmentDiv"></p>
                                     <button type="button" data-wit-id="${data[i].id}" class="wit-like-btn wit-like-btn-${data[i].id} btn btn-default btn-sm">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
                                             <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
@@ -136,7 +137,7 @@ jQuery(function() {
                         if (data[i].image) {
                             // Assuming images are stored in the "public/uploads/" directory
                             var imageUrl = `/uploads/${data[i].image}`;
-                            row.append(`<img src="${imageUrl}" alt="Wit Image" class="wit-image">`);
+                            row.find(".imgAttachmentDiv").html(`<img src="${imageUrl}" alt="Wit Image" class="wit-image">`);
                         }
 
                         $("#wits-area").prepend(row);
@@ -209,6 +210,7 @@ jQuery(function() {
                             <div class="col-md-9">
                                 <h4 class="T-wit-author">@${data[i].author}</h4><p class="T-wit-date">${moment(data[i].createdAt).format("h:mma on dddd")} </p>
                                 <p class="T-wit-body">${data[i].body}</p>
+                                <p class="imgAttachmentDiv"></p>
                                 <button type="button" data-wit-id="${data[i].id}" class="wit-like-btn btn btn-default btn-sm">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
                                         <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
@@ -243,7 +245,7 @@ jQuery(function() {
                         if (data[i].image) {
                             // Assuming images are stored in the "public/uploads/" directory
                             var imageUrl = `/uploads/${data[i].image}`;
-                            row.append(`<img src="${imageUrl}" alt="Wit Image" class="wit-image">`);
+                            row.find(".imgAttachmentDiv").html(`<img src="${imageUrl}" alt="Wit Image" class="wit-image">`);
                         }
 
                         $("#T-wits-area").prepend(row);
