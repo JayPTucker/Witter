99d6d13a0340fefba900044c04bf3034cf64be10
Jay Paul Tucker <jaypaultucker@gmail.com>
2024-10-05 17:44:34 -0500

    Server.js Revert
    - Reverted back for now.

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-require('dotenv').config(); // Ensure this is at the top of your server.js file
-const express = require("express");
-const compression = require('compression');
-const session = require("express-session");
-const passport = require("./config/passport");
-const path = require("path");
-const { Sequelize } = require('sequelize');
-const config = require('./config/config.js'); // Import the config file
+// DEPENDENCIES
+require("dotenv").config();
 
+var express = require("express");
+var compression = require('compression')
+var session = require("express-session");
+var passport = require("./config/passport");
+var path = require("path"); // Import the 'path' module
 // =====================================
 
 // Sets up the Express app
-const PORT = process.env.PORT || 8080;
-const app = express();
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
+var app = express();
 // =====================================
 
 app.use(compression());
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 // =====================================
 
@@ -29,16 +29,6 @@ app.use(session({ secret: process.env.SECRET, resave: true, saveUninitialized: t
 app.use(passport.initialize());
 app.use(passport.session());
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
-  }
-);
-
 // Routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
@@ -48,16 +38,9 @@ app.get('/verificationCode', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'verificationCode.html'));
 });
 // =====================================
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
 
 // Starts the server to begin Listening:
-sequelize.sync().then(function() {
+db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
         console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
     });
